27328c9888026642a8f5342d21935e3eb13555e7
Benjamin Paredes <paredesbenjamin452@gmail.com>
2025-06-12 21:19:59 -0300

    avance paquete

--- Project/src/app/app.component.ts
+++ Project/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { HeaderComponent } from './components/Header/header.component';
+import { PaqueteComponent } from './views/Section/paquete.component'
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, PaqueteComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ Project/src/app/views/Section/paquete.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-paquete',
+  imports: [],
+  templateUrl: './paquete.component.html',
+  styleUrl: './paquete.component.scss'
+})
+export class PaqueteComponent {
+
+}
