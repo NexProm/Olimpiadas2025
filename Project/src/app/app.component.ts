24e7129c594e98ae1fcaa54077d59ae00d6598ec
arielortega2 <arielboca2007@gmail.com>
2025-06-13 21:53:06 -0300

    Diseño y codificación de Header, Menú, Footer y el formulario de selección de paquetes turísticos

--- Project/src/app/app.component.ts
+++ Project/src/app/app.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from "./views/header/header.component";
+import { FooterComponent } from "./views/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,CommonModule, RouterModule],
+  imports: [RouterOutlet, CommonModule, RouterModule, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
