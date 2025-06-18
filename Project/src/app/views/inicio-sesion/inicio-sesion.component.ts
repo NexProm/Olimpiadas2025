5e680d99f3b112533796b91972502dbc1ccfd8ff
favex2007 <favcarchivos@gmail.com>
2025-06-17 21:59:37 -0300

    Agregacion de inicio de sesion

---
+++ Project/src/app/views/inicio-sesion/inicio-sesion.component.ts
@@ -0,0 +1,36 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { FormService } from '../../services/form.service';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-inicio-sesion',
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './inicio-sesion.component.html',
+  styleUrl: './inicio-sesion.component.scss'
+})
+export class InicioSesionComponent implements OnInit {
+  Form!: FormGroup
+  form= new FormService();
+  router = inject(Router)
+
+  ngOnInit(): void {
+    this.Form = new FormGroup({
+      name: new FormControl( "",[Validators.required, Validators.maxLength(20)]),
+      email: new FormControl("", [Validators.required, Validators.maxLength(100), Validators.email]),
+      password: new FormControl("", [Validators.required, Validators.minLength(8), Validators.maxLength(20)]),
+    });
+
+  }
+
+  validation() {
+
+    if(!this.Form.valid){
+      alert("Faltan datos para llenar");
+      return;
+    }
+
+    this.router.navigate(['/gratitude']);
+
+} } 
