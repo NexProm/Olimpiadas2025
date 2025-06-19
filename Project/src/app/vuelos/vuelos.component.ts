1cb028d6054f5070a3570e37b7b14d6519406c53
Benji2672 <paredesbenjamin452@gmail.com>
2025-06-19 18:20:38 -0300

    Nuevas subpáginas

---
+++ Project/src/app/vuelos/vuelos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vuelos',
+  imports: [],
+  templateUrl: './vuelos.component.html',
+  styleUrl: './vuelos.component.scss'
+})
+export class VuelosComponent {
+
+}
