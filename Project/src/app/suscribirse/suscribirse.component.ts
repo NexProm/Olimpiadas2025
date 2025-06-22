757a1747b829fe6297e07bde15745c74f0a9d1e6
facundovegacruz <vegacruzfacundo@gmail.com>
2025-06-22 13:23:54 -0300

    Nuevas páginas

---
+++ Project/src/app/suscribirse/suscribirse.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-suscribirse',
+  imports: [],
+  templateUrl: './suscribirse.component.html',
+  styleUrl: './suscribirse.component.scss'
+})
+export class SuscribirseComponent {
+
+}
---
+++ Project/src/app/views/jefes/jefes.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-jefes',
+  imports: [],
+  templateUrl: './jefes.component.html',
+  styleUrl: './jefes.component.scss'
+})
+export class JefesComponent {
+
+}
